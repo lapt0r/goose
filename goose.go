b019ff4b80a8c67a60a774fa21d580db239a4e46
Morgan <morgan.roman@protonmail.com>
2020-01-05 21:41:43 -0800

    fixed all places

--- goose.go
+++ goose.go
@@ -32,7 +32,7 @@ func printHeader() {
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		fmt.Println("%v", scanner.Text())
 	}
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
