41838b7bcda14baa14b2cda1dc86bac81b4eb716
Kurt Boberg <kboberg@chegg.com>
2020-06-04 11:34:39 -0700

    finally fix scanning data race on > 8196 targets (goroutine limit)

--- internal/app/app.go
+++ internal/app/app.go
@@ -8,6 +8,7 @@ import (
 	"regexp"
 	"strings"
 	"sync"
+	"time"
 
 	"github.com/lapt0r/goose/internal/pkg/configuration"
 	"github.com/lapt0r/goose/internal/pkg/decisionfilter"
@@ -35,31 +36,24 @@ func Init(configpath string, targetpath string, interactive bool, commitDepth in
 	rules = configuration.LoadConfiguration(configpath)
 	absoluteTargetPath, _ = filepath.Abs(targetpath)
 	if interactive {
-		fmt.Printf("Initializing Goose with target [%v]..\n", absoluteTargetPath)
+		fmt.Printf("[%v] Initializing Goose with target [%v]..\n", time.Now().Format(time.RFC1123), absoluteTargetPath)
 	}
 	targets = loader.GetTargets(absoluteTargetPath, commitDepth)
 	if interactive {
-		fmt.Printf("Got [%v] targets\n", len(targets))
+		fmt.Printf("[%v] Got [%v] targets\n", time.Now().Format(time.RFC1123), len(targets))
 	}
 }
 
 //Run : runs the Goose application
 func Run(interactive bool, decisionTree bool, outputmode string, filterPaths string) {
-	var fChannel = make(chan []finding.Finding, 1)
-
+	//todo: configurable concurrency
+	var concurrency = 400
+	var fChannel = make(chan []finding.Finding, concurrency)
+	var semaphore = make(chan bool, concurrency)
 	var results []finding.Finding
 	var wg sync.WaitGroup
 	var total = len(targets)
 	var count = 0
-	for _, target := range targets {
-		wg.Add(1)
-		if decisionTree {
-			go decisionfilter.ScanFile(target, fChannel, &wg)
-		} else {
-			go regexfilter.ScanFile(target, &rules, fChannel, &wg)
-		}
-	}
-
 	go func() {
 		defer func() {
 			if r := recover(); r != nil {
@@ -67,7 +61,7 @@ func Run(interactive bool, decisionTree bool, outputmode string, filterPaths str
 			}
 		}()
 		if interactive {
-			fmt.Printf("Waiting for routines to finish..\n")
+			fmt.Printf("[%v] Waiting for routines to finish..\n", time.Now().Format(time.RFC1123))
 		}
 		for {
 			result, _ := <-fChannel
@@ -84,6 +78,21 @@ func Run(interactive bool, decisionTree bool, outputmode string, filterPaths str
 		}
 
 	}()
+	for _, target := range targets {
+		wg.Add(1)
+		semaphore <- true
+		go func(t loader.ScanTarget, w *sync.WaitGroup) {
+			defer func() { <-semaphore }()
+			if decisionTree {
+				decisionfilter.ScanFile(t, fChannel, &wg)
+			} else {
+				regexfilter.ScanFile(t, &rules, fChannel, &wg)
+			}
+		}(target, &wg)
+	}
+	for i := 0; i < cap(semaphore); i++ {
+		semaphore <- true
+	}
 	wg.Wait()
 	results = filterResults(results, strings.Split(filterPaths, ","))
 	outputResults(results, interactive, outputmode)
@@ -105,7 +114,7 @@ func filterResults(findings []finding.Finding, filterPaths []string) []finding.F
 func outputResults(result []finding.Finding, interactive bool, outputmode string) {
 	if interactive {
 		fmt.Println("\n--- Scanning complete ---")
-		fmt.Printf("[%v] results\n", len(result))
+		fmt.Printf("[%v] [%v] results\n", time.Now().Format(time.RFC1123), len(result))
 		for _, finding := range result {
 			fmt.Printf("FINDING\n-----\n%v\n", finding)
 		}
